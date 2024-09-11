b383b49ea32ccdca3848eb552cab9226fce9d700
djordjestanisic <djostanisic@gmail.com>
2024-09-11 15:26:12 +0200

    change splash screen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 
 class SplashScreen extends ConsumerWidget {
@@ -13,14 +14,54 @@ class SplashScreen extends ConsumerWidget {
 
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.onPrimary,
-      body: Center(
-        child: Image.asset(
-          'lib/assets/splash_screen_app_icon.png', // Path to your SVG asset
-          width: 180, // Specify the size of the icon
-          height: 180,
-        ),
+      body: Column(
+        children: [
+          const Spacer(), // Pushes the content in the center vertically
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min, // Ensures the content takes minimum vertical space
+              children: [
+                SvgPicture.asset(
+                  'lib/assets/splash_screen_app_icon.svg', // Path to your asset
+                  width: 150,
+                  height: 150,
+                ),
+                const SizedBox(height: 8), // Reduced space between image and text
+                const Text(
+                  'Red Vožnje', // Normal text
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 4), // Reduced space between the texts
+                const Text(
+                  'Novi Sad', // Larger and bold text
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(), // Pushes the content towards the center
+          const Padding(
+            padding: EdgeInsets.only(bottom: 20), // Bottom padding for the text
+            child: Text(
+              'Powered by Crystal Pigeon', // Text at the bottom
+              style: TextStyle(
+                fontSize: 10,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
     );
+
+
   }
 }
 
@@ -28,5 +69,5 @@ Future<void> _goToHomePage(BuildContext context) async {
   await Future.delayed(const Duration(
     milliseconds: 1000 ,
   ));
-  context.goNamed('homePage');
+  // context.goNamed('homePage');
 }
